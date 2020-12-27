ee10d65278220ddc84230caf04c4621cf6cb5107
Addison Dalton <zarch14@gmail.com>
2020-12-27 12:00:33 -0500

    setup for distance and direction

---
+++ Assets/Scripts/GameTile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTile : MonoBehaviour
+{
+  [SerializeField]
+  Transform arrow = default;
+  GameTile north, east, south, west, nextOnPath;
+  int distance;
+  public bool HasPath => distance != int.MaxValue;
+
+  public static void MakeEastWestNeighbors (GameTile east, GameTile west) {
+    Debug.Assert(west.east == null && east.west == null, "Redefined EastWest neighbors");
+    west.east = east;
+    east.west = west;
+  }
+  public static void MakeNorthSouthNeights (GameTile north, GameTile south) {
+    Debug.Assert(north.south == null && south.north == null, "Redefined NorthSouth neighbors");
+    north.south = south;
+    south.north = north;
+  }
+
+  public void ClearPath () {
+    distance = int.MaxValue;
+    nextOnPath = null;
+  }
+
+  public void BecomeDestination () {
+    distance = 0;
+    nextOnPath = null;
+  }
+}
